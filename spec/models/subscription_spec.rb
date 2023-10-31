e9f823593345dc36e9ebb6339f8288d88c8a2a97
Kaina Cockett <kainacockett@gmail.com>
2023-10-31 17:58:38 -0600

    test: create model tests

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,15 @@
+include 'rails_helper'
+
+Rspec.describe Subscription, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price)}
+    it { should validate_presence_of(:status)}
+    it { should validate_uniqueness_of(:frequency)}
+  end
+
+  describe 'relationships' do
+    it { should have_many(:teas)}
+    it { should have_many(:customers)}
+  end
+end
