0348d5b86a6b075bd5710f7af60d3581c78b955f
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 14:59:09 -0600

    feat: create api to set subscription to cancelled

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -9,13 +9,26 @@ class Api::V0::SubscriptionsController < ApplicationController
     end
   end
 
-  def delete
-
+  def destroy
+    subscription = Subscription.find(removal_params[:id])
+    if subscription.customer_id == removal_params[:customer_id].to_i
+      if subscription.update(status: "cancelled")
+        render json: { message: 'Subscription successfully cancelled' }, status: :ok
+      else
+        # Future sad path logic... render json: { error: 'Failed to update the subscription status' }, status: :unprocessable_entity
+      end
+    else
+      # Future sad path logic... render json: { error: 'Cannot be processed' }, status: :422
+    end
   end
 
   private
 
   def subscription_params
-    params.require(:subscription).permit(:id, :title, :price, :customer_id, :tea_id, :status, :frequency_months)
+    params.require(:subscription).permit(:title, :price, :customer_id, :tea_id, :status, :frequency_months)
+  end
+
+  def removal_params
+    params.require(:subscription).permit(:id, :customer_id)
   end
 end
