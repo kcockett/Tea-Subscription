c98d45b13dec163cece70e7e025b3ae1a5c6b83d
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 14:16:36 -0600

    test: create test for sub removal api

---
+++ spec/requests/api/v0/remove_subscription_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Remove Subscription', type: :request do
+  describe 'with valid params' do
+    it 'removes a subscription' do
+      customer = create(:customer)
+      tea = Tea.all.first
+      subscription = Subscription.create!(title: "Monthly English Breakfast", price: 7.95, customer_id: customer.id, tea_id: tea.id, status: "active", frequency_months: 1)
+      valid_params = { subscription: { id: subscription.id }}
+
+      delete "/api/v0/subscriptions", params: valid_params
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "without valid params" do
+    # Future sad path tests
+  end
+end
