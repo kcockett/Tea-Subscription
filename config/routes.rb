1d9a8da9442057e0953ebb4c736100b09191c8aa
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 17:50:24 -0600

    fix: change routes to RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :customers, only: [:create] do
-        resources :subscriptions, only: [:index]
+        resources :subscriptions, only: [:index, :create, :update]
       end
-      resources :subscriptions, only: [:create, :destroy]
     end
   end
 end
