146d55141609b8029474c30e832d4e50f5bf250d
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 14:57:14 -0600

    fix: tests updated

--- spec/requests/api/v0/remove_subscription_spec.rb
+++ spec/requests/api/v0/remove_subscription_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe 'Remove Subscription', type: :request do
       customer = create(:customer)
       tea = Tea.all.first
       subscription = Subscription.create!(title: "Monthly English Breakfast", price: 7.95, customer_id: customer.id, tea_id: tea.id, status: "active", frequency_months: 1)
-      valid_params = { subscription: { id: subscription.id }}
+      valid_params = { subscription: { id: subscription.id, customer_id: customer.id }}
 
       delete "/api/v0/subscriptions/#{subscription.id}", params: valid_params
 
       expect(response).to have_http_status(:ok)
+      
+      updated_subscription = Subscription.find(subscription.id)
+      expect(updated_subscription.status).to eq("cancelled")
     end
   end
 
