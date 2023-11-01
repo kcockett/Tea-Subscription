b12806a701f3a19cf9e8768a03d13b9965c76e2a
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 10:44:37 -0600

    test: add subscription files

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price)}
     it { should validate_presence_of(:status)}
-    it { should validate_presence_of(:frequency)}
+    it { should validate_presence_of(:frequency_months)}
     it { should validate_presence_of(:customer_id)}
     it { should validate_presence_of(:tea_id)}
   end
