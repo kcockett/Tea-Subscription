9ebe5a06a9360bd14aa04406331cbffe9df0c02c
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 13:40:10 -0600

    test: add subscription api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+require 'faker'
+require 'factory_bot_rails'
+FactoryBot::SyntaxRunner.send(:include, FactoryBot::Syntax::Methods)
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/rails_helper.rb'
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -20,7 +30,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -30,6 +40,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
---
+++ spec/requests/api/v0/add_subscription_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v0/subscriptions', type: :request do
+  describe 'with valid params' do
+    it 'creates a new subscription' do
+      customer = create(:customer)
+      tea_selection = Tea.all.first
+      valid_params = { subscription: { customer_id: customer.id, tea_id: tea_selection.id, status: "active", frequency: 1 } }
+      post '/api/v0/subscriptions', params: valid_params
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+  
+  describe 'handles subscription duplication' do
+    it 'returns bad request when email is not unique' do
+      # Future sad path tests
+    end
+  end
+
+  describe 'with invalid params' do
+    it 'returns bad request when missing customer_id' do
+      # Future sad path tests
+    end
+  end
+end
