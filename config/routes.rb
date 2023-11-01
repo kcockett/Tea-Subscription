65422b6f8ddce50dc0958e1b91c680bfa3ff6f4d
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 14:18:53 -0600

    add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :customers, only: [:create]
-      resources :subscriptions, only: [:create]
+      resources :subscriptions, only: [:create, :destory]
     end
   end
 end
