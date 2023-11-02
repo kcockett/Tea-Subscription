19df34ef7bd7337ce35023a45212d38fe7709715
Kaina Cockett <kainacockett@gmail.com>
2023-11-01 18:27:58 -0600

    fix: clean-up diagnostic code

--- spec/requests/api/v0/get_tea_spec.rb
+++ spec/requests/api/v0/get_tea_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'GET /api/v0/teas', type: :request do
       get '/api/v0/teas'
       all_teas = JSON.parse(response.body, symbolize_names: true)
 
-      require "pry"; binding.pry
       expect(response).to have_http_status(:ok)
       expect(all_teas.count).to eq(10)
     end
