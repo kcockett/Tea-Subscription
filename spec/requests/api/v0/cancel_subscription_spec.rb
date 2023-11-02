836ab3e8647f4ead8a9231187dac1077dfbaab4d
Kaina Cockett <kainacockett@gmail.com>
2023-11-02 11:54:38 -0600

    fix: sad path and spelling

--- spec/requests/api/v0/cancel_subscription_spec.rb
+++ spec/requests/api/v0/cancel_subscription_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Cancel Subscription', type: :request do
       expect(response).to have_http_status(:ok)
       
       updated_subscription = Subscription.find(subscription.id)
-      expect(updated_subscription.status).to eq("cancelled")
+      expect(updated_subscription.status).to eq("canceled")
     end
   end
 
@@ -25,6 +25,26 @@ RSpec.describe 'Cancel Subscription', type: :request do
       valid_params = { subscription: { status: "cancel"}}
 
       patch "/api/v0/customers/9999999999/subscriptions/#{subscription.id}", params: valid_params
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+    it "returns an error if given an incorrect subscription id" do
+      customer = create(:customer)
+      tea = Tea.all.first
+      subscription = Subscription.create!(title: "Monthly English Breakfast", price: 7.95, customer_id: customer.id, tea_id: tea.id, status: "active", frequency_months: 1)
+      valid_params = { subscription: { status: "cancel"}}
+
+      patch "/api/v0/customers/#{customer.id}/subscriptions/99999999999", params: valid_params
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+    it "returns an error if given an incorrect status" do
+      customer = create(:customer)
+      tea = Tea.all.first
+      subscription = Subscription.create!(title: "Monthly English Breakfast", price: 7.95, customer_id: customer.id, tea_id: tea.id, status: "active", frequency_months: 1)
+      valid_params = { subscription: { status: "bad status"}}
+
+      patch "/api/v0/customers/#{customer.id}/subscriptions/#{subscription.id}", params: valid_params
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
